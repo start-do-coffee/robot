c06827f764408b085bb16ce41d355f7698ef487b
泰森 <my556655662000@gmail.com>
2023-04-11 00:07:28 +0800

    test

--- root/routerIndex.php
+++ root/routerIndex.php
@@ -3,23 +3,40 @@
 
     $router = new AltoRouter();
 
-/************************************
-    * 函式簡述： 取得App初始資料
-    * 路徑：/init
-    * Method：GET
-    * 輸入參數 ：Null
-    * @return：JSON Array
-***********************************/
-    //$router->map( 'GET', '/login/line', array(new LineAccountCotroller(),'login') ,'LineLogin');
-
-
-    setAccountRouter($router);
 
 
 
-    function setAccountRouter($router){
+    setLineAccountRouter($router);
 
+
+
+    function setLineAccountRouter($router){
+
+    /************************************
+        * 函式簡述： 取得App初始資料
+        * 路徑：/init
+        * Method：GET
+        * 輸入參數 ：Null
+        * @return：JSON Array
+    ***********************************/
         $router->map( 'GET', '/line/login', array(new LineAccountCotroller(),'login') ,'LineLogin');
+    /************************************
+        * 函式簡述： 取得App初始資料
+        * 路徑：/init
+        * Method：GET
+        * 輸入參數 ：Null
+        * @return：JSON Array
+    ***********************************/
+        $router->map( 'GET', '/line/logout', array(new LineAccountCotroller(),'login') ,'LineLogOut');
+    
+    /************************************
+        * 函式簡述： 取得App初始資料
+        * 路徑：/init
+        * Method：GET
+        * 輸入參數 ：Null
+        * @return：JSON Array
+    ***********************************/
+        $router->map( 'GET', '/line/data', array(new LineAccountCotroller(),'login') ,'getAccountData');
 
         return $router;
     }
