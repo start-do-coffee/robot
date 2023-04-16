affa4bbc4634f38630f5b79a84e7ad3329c26d90
泰森 <my556655662000@gmail.com>
2023-04-16 22:39:24 +0800

    簡易問答機器人完成

--- root/router/routerRobot.php
+++ root/router/routerRobot.php
@@ -4,9 +4,9 @@ function setRobotRouter($router){
 
 /************************************
     * 函式簡述： 機器人及問及回答
-    * 路徑：/robot/ask
+    * 路徑：/robot/question/ask/[*:keyWord]
     * Method：GET
-    * 輸入參數 ：Null
+    * 輸入參數 ：keyWord => 需查詢的問句或是關鍵字
     * @return：JSON Array
 ***********************************/
     $router->map( 'GET', '/robot/question/ask/[*:keyWord]', array(new cotrollerChatGPT(),'askQuestion') ,'RobotAskQuestion');
