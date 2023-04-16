b77574a74ed9138b518024b446b32fec8b04bead
Replit user <>
2023-04-16 09:51:40 +0000

    可以進行搜尋及印出表單內容

--- models/account/ModelLineAccount.php
+++ models/account/ModelLineAccount.php
@@ -10,12 +10,65 @@ class ModelLineAccount {
 ***********************************/
     function login(){
         echo "Model Line Login";
-        $KEY = "";
-        $SheetIndex = 1;
-        $json_url = "https://spreadsheets.google.com/feeds/list/".$KEY."/".$SheetIndex."/public/values?alt=json";
-        $string = file_get_contents($json_url);
-        $data_json = json_decode($string,true);
-        print_r($data_json);
+        // 建立 Google Client
+$client = new \Google_Client();
+$client->setApplicationName('Google Sheets and PHP');
+// 設定權限
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offline');
+// 引入金鑰
+$client->setAuthConfig(__DIR__ . '/credentials.json');
+
+// 建立 Google Sheets Service
+$service = new \Google_Service_Sheets($client);
+
+// Google Sheet ID
+$spreadsheetId = '18e1rZdQDRB-x6-q40IE56ca1sWmfsu9fVAyyWaojNYI';
+// 取得 Sheet 範圍
+$getRange = "start-do-ask!A1:L20";
+
+// 讀取資料
+$response = $service->spreadsheets_values->get($spreadsheetId, $getRange);
+$values = $response->getValues();
+
+$searchValue = '2023-04-16'; // 要搜尋的值
+
+$found = false; // 設定一個旗標來表示是否有找到對應的值
+
+// 迴圈搜尋
+foreach ($values as $row) {
+    if (isset($row[0]) && count($row) >= 2 && $row[0] == $searchValue) {
+        echo "<br><br><br><br>問句是: ";
+        echo "$searchValue";
+        echo "<br><br><br><br>回答是: ";
+        echo $row[1]; // 輸出對應的第二欄資料
+        $found = true;
+
+        break;
+    }
+
+}
+
+if (!$found) {
+    echo "找不到對應的資料";
+}
+if (empty($values)) {
+    print "No data found.\n";
+} else {
+    foreach ($values as $row) {
+        $date = isset($row[0]) ? $row[0] : '';
+        $question = isset($row[1]) ? $row[1] : '';
+        $answer = isset($row[2]) ? $row[2] : '';
+
+        printf("日期：%s，問題：%s，答案：%s<br>", $date, $question, $answer);
+    }
+}
+      
+
+
+      
+
+      
     }
 /************************************
     * 函式簡述： 登出
